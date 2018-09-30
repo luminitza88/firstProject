90740b66d4f0dfd775276733978cdb0836e6ff63
sylviancb <silviu.ciupitu@gmail.com>
2018-09-30 11:18:22 +0300

    adsfadsf

--- src/firstProject/Main.java
+++ src/firstProject/Main.java
@@ -5,11 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello Lumi");
 		System.out.println("bla bla");
-		System.out.println("bla bla");
-		System.out.println("bla bla");
-		System.out.println("sldkfglsdijfg");
-		System.out.println("sldkfglsdijfg");
-
 	}
 
 	public void newMethod() {
